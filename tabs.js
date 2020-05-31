5e5ba73891edcbccc53065fb33d5ae20397105c1
Muneeb Ajaz <Muneeb Ajaz@DELL>
2020-05-31 12:37:29 +0500

    Fixed Bug with Sheet Buttons

--- tabs.js
+++ tabs.js
@@ -1,21 +1,9 @@
- document.addEventListener("click", (e) => {
+document.addEventListener("click", (e) => {
 
-     if (e.target.id === "tabs-create-tag-checker") {
-         browser.tabs.create({ url: "http://wwwtags.net" });
-     } else if (e.target.id === "tabs-create-sheet") {
-         browser.tabs.create({ url: "http://wwwtags.net/Sheet" });
-
-         browser.tabs.query({
-             currentWindow: true
-         }).then((tabs) => {
-             for (var tab of tabs) {
-                 if (tab.id === tabId) {
-                     browser.tabs.update(tabId, {
-                         active: true
-                     });
-                 }
-             }
-         });
-     }
-     e.preventDefault();
- });
+    if (e.target.id === "tabs-create-tag-checker") {
+        browser.tabs.create({ url: "http://wwwtags.net" });
+    } else if (e.target.id === "tabs-create-sheet") {
+        browser.tabs.create({ url: "http://wwwtags.net/Sheet" });
+    }
+    e.preventDefault();
+});
